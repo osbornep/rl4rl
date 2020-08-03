753dfc4ea19d54b5b808b10232d72d2d043bf462
Philip Osborne <kr015604@gmail.com>
2020-08-03 09:58:07 +0100

    Update Q_learning.py

--- Classroom/Q_learning.py
+++ Classroom/Q_learning.py
@@ -6,9 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib.pyplot import figure
 
-from IPython.display import clear_output
-from IPython import display
-
+import tqdm
 import time
 
 import warnings
@@ -26,12 +24,15 @@ sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 5})
 sns.set_style("whitegrid", {'axes.grid' : False})
 
 #------------------------------------------------------------------------------------------
-
-# Define states with 1) Labels, 2) Rewards and 3) [x,y] coordinates
+# ENVIRONMENT REQUIREMENTS
+## Define states with 1) Labels, 2) Rewards and 3) [x,y] coordinates
 states = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'T', 'M', 'recycling', 'trash']
 rewards = [-0.04, -0.04, -0.04, -0.04, -0.04, -0.04, -0.04, -0.04, -0.04, 1, -1]
 x_list = [4,3,2,1,1,1,2,3,3,4,4]
 y_list = [1,1,1,1,2,3,3,3,2,3,2]
 
-# The low-level actions the agent can make in the environment
+## The low-level actions the agent can make in the environment
 actions = ['left','right','up','down']
+
+
+
